0154cedc8ddf92da4197a61c08b98faceacaee59
antoniochi90 <160528309+antoniochi90@users.noreply.github.com>
2024-07-01 20:06:17 -0600

    Update register.dart

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,45 +1,397 @@
-import 'package:digitalevent/pages/home.dart';
 import 'package:digitalevent/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Register extends StatefulWidget {
-  const Register({super.key});
-
   @override
   State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
+  // Variable para el valor seleccionado
+  String? selectedGender;
+
+  // Lista de opciones
+  final List<String> genderOptions = ['Masculino', 'Femenino', 'Otro'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Registro / Antonio", style: TextStyle(fontSize: 20.0),),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Home(),));
-              }, 
-              style: const ButtonStyle(
-               backgroundColor: WidgetStatePropertyAll<Color>(Colors.deepPurple),
-                 ),
-              child: Text('Home', style: TextStyle(color: Colors.white),),
-              
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/login.png'),
+                fit: BoxFit.cover,
               ),
-               ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Login(),));
-              }, 
-              style: const ButtonStyle(
-               backgroundColor: WidgetStatePropertyAll<Color>(Colors.deepPurple),
-                 ),
-              child: Text('Login', style: TextStyle(color: Colors.white),),
-              
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "Registrate",
+                    style: GoogleFonts.lato(
+                      // Aplica la fuente Google
+                      fontSize: 28.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Crea tu cuenta ahora mismo',
+                    style:
+                        GoogleFonts.roboto(color: Colors.white, fontSize: 17),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: 310,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromARGB(225, 185, 166, 224),
+                          labelText: 'Nombre Completo:',
+                          labelStyle: GoogleFonts.openSans(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w700),
+                          enabledBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.deepPurple,
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          border: const OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 310,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(225, 185, 166, 224),
+                        labelText: 'Correo Electrónico:',
+                        labelStyle: GoogleFonts.openSans(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(225, 185, 166, 224),
+                            labelText: 'Contraseña:',
+                            labelStyle: GoogleFonts.openSans(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.deepPurple,
+                                  style: BorderStyle.solid,
+                                  width: 2.0,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Color.fromARGB(225, 185, 166, 224),
+                            labelText: 'Confirmar contraseña:',
+                            labelStyle: GoogleFonts.openSans(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.deepPurple,
+                                  style: BorderStyle.solid,
+                                  width: 2.0,
+                                ),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15))),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 310,
+                    child: DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(225, 185, 166, 224),
+                        labelText: 'Sexo:',
+                        labelStyle: GoogleFonts.openSans(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                      ),
+                      value: selectedGender,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedGender = newValue;
+                        });
+                      },
+                      items: genderOptions
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 310,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(225, 185, 166, 224),
+                        labelText: 'Edad:',
+                        labelStyle: GoogleFonts.openSans(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 310,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(225, 185, 166, 224),
+                        labelText: 'Localidad:',
+                        labelStyle: GoogleFonts.openSans(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 310,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color.fromARGB(225, 185, 166, 224),
+                        labelText: 'Ciudad:',
+                        labelStyle: GoogleFonts.openSans(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              style: BorderStyle.solid,
+                              width: 2.0,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 270,
+                    child: SizedBox(
+                      height: 45,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Login(),
+                              )); //
+                        },
+                        style: const ButtonStyle(
+                          backgroundColor:
+                              WidgetStatePropertyAll<Color>(Colors.deepPurple),
+                        ),
+                        child: Text(
+                          'Registrase',
+                          style: GoogleFonts.openSans(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    'O',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Container(
+                    width: 305,
+                    child: SizedBox(
+                      height: 45,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                          backgroundColor:
+                              WidgetStatePropertyAll<Color>(Colors.white),
+                          shape: WidgetStatePropertyAll<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)))),
+                          side: WidgetStatePropertyAll<BorderSide>(
+                              BorderSide(width: 2.0, color: Colors.deepPurple)),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              'Registrarse Con Google',
+                              style: GoogleFonts.openSans(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const Image(image: AssetImage('assets/google.png')),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 40,
+                      ),
+                      Text(
+                        'Ya tienes una Cuenta?',
+                        style: GoogleFonts.openSans(
+                            fontSize: 17, fontWeight: FontWeight.w500),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Aquí puedes añadir la lógica que quieres ejecutar al presionar el botón
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Login()));
+                        },
+                        child: Text(
+                          'Inicia Sesión',
+                          style: GoogleFonts.openSans(
+                              fontSize: 17,
+                              color: Colors.deepPurple,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  )
+                ],
               ),
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
