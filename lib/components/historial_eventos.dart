0f10c67c72e125879f3ab06c8ef973ab2fc4cbf1
Christian Navarro <christianyahirnavarrochan@gmail.com>
2024-07-03 21:25:15 -0600

    cambios

---
+++ lib/components/historial_eventos.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+class HistorialEventos extends StatefulWidget {
+  const HistorialEventos({super.key});
+
+  @override
+  State<HistorialEventos> createState() => _HistorialEventosState();
+}
+
+class _HistorialEventosState extends State<HistorialEventos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        backgroundColor: Colors.deepPurple[300],
+        elevation: 0,
+        title: Text("Historial de eventos", textAlign: TextAlign.center,),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[200]
+                ),
+                child: Row(
+                  children: [
+                    Image.network(
+                      "https://www.facilitymanagementservices.es/wp-content/uploads/sites/3/2020/10/logo-smart-city-1200x630.png",
+                      width: 150,
+                      ),
+                       Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Smart City",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 8), // Espacio entre el título y el texto
+                    Text(
+                      "Este año el Smart City Expo LATAM Congress busca a través del liderazgo y el compromiso comunitario, que nuestras ciudades en Latinoamérica se transformen en entornos más inteligentes y sostenibles.",
+                        style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+                  ],
+                ),
+              ),
+            ),
+                  Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[200]
+                ),
+                child: Row(
+                  children: [
+                    Image.network(
+                      "https://www.facilitymanagementservices.es/wp-content/uploads/sites/3/2020/10/logo-smart-city-1200x630.png",
+                      width: 150,
+                      ),
+                       Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Smart City",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 8), // Espacio entre el título y el texto
+                    Text(
+                      "Este año el Smart City Expo LATAM Congress busca a través del liderazgo y el compromiso comunitario, que nuestras ciudades en Latinoamérica se transformen en entornos más inteligentes y sostenibles.",
+                        style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+                  ],
+                ),
+              ),
+            ),
+                  Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[200]
+                ),
+                child: Row(
+                  children: [
+                    Image.network(
+                      "https://www.facilitymanagementservices.es/wp-content/uploads/sites/3/2020/10/logo-smart-city-1200x630.png",
+                      width: 150,
+                      ),
+                       Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Smart City",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 8), // Espacio entre el título y el texto
+                    Text(
+                      "Este año el Smart City Expo LATAM Congress busca a través del liderazgo y el compromiso comunitario, que nuestras ciudades en Latinoamérica se transformen en entornos más inteligentes y sostenibles.",
+                        style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+                  ],
+                ),
+              ),
+            ),
+                  Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[200]
+                ),
+                child: Row(
+                  children: [
+                    Image.network(
+                      "https://www.facilitymanagementservices.es/wp-content/uploads/sites/3/2020/10/logo-smart-city-1200x630.png",
+                      width: 150,
+                      ),
+                       Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Smart City",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 8), // Espacio entre el título y el texto
+                    Text(
+                      "Este año el Smart City Expo LATAM Congress busca a través del liderazgo y el compromiso comunitario, que nuestras ciudades en Latinoamérica se transformen en entornos más inteligentes y sostenibles.",
+                        style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+                  ],
+                ),
+              ),
+            ),
+                  Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[200]
+                ),
+                child: Row(
+                  children: [
+                    Image.network(
+                      "https://www.facilitymanagementservices.es/wp-content/uploads/sites/3/2020/10/logo-smart-city-1200x630.png",
+                      width: 150,
+                      ),
+                       Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Smart City",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 8), // Espacio entre el título y el texto
+                    Text(
+                      "Este año el Smart City Expo LATAM Congress busca a través del liderazgo y el compromiso comunitario, que nuestras ciudades en Latinoamérica se transformen en entornos más inteligentes y sostenibles.",
+                        style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
