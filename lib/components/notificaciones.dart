4eb353e3381c1b09b1ba5b0e77714b147720dab1
D3nn1S-Mugart3 <dennis.mugarte05@gmail.com>
2024-07-09 08:39:08 -0600

    cambios

--- lib/components/notificaciones.dart
+++ lib/components/notificaciones.dart
@@ -94,33 +94,58 @@ class _NotificacionesState extends State<Notificaciones> {
       body: ListView.builder(
         itemCount: notifications.length,
         itemBuilder: (context, index) {
-          return Card.outlined(
-            borderOnForeground: false,
+          return Card(
+            elevation: 12,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(10),
               side: const BorderSide(
                 color: Color.fromRGBO(186, 104, 200, 1),
               ),
             ),
             color: Colors.white,
-            margin: EdgeInsets.all(10),
-            child: ListTile(
-              leading: Image.network(
-                notifications[index]['image']!,
-                width: 50,
-                height: 50,
-                fit: BoxFit.cover,
-              ),
-              title: Text(
-                notifications[index]['title']!,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.purple[300],
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
-              subtitle: Text(
-                notifications[index]['description']!,
+            margin: const EdgeInsets.all(10),
+            child: Container(
+              height: 130,
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Container(
+                    width: 90,
+                    height: double.infinity,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        notifications[index]['image']!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          notifications[index]['title']!,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple[300],
+                            fontSize: 17,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          notifications[index]['description']!,
+                          style: TextStyle(fontSize: 15),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 3,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           );
